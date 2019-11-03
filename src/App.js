53a0479d66828d177b47f4603420a6419908fd2f
Toma <docsperj@gmail.com>
2019-11-03 10:17:30 +0300

    creature components 2

--- src/App.js
+++ src/App.js
@@ -1,52 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Container from './components/Container';
 
 const App = () => {
     return (
         <div className="app-wrapper">
-            <header className="header">
-                <img src="https://upload.wikimedia.org/wikipedia/ru/2/24/WWF_logo.svg" />
-            </header>
-            <nav className="nav">
-                <div>
-                    <a>Profile</a>
-                </div>
-                <div>
-                   <a>Messages</a>
-                </div>
-                <div>
-                   <a>News</a>
-                </div>
-                <div>
-                    <a>Music</a>
-                </div>
-                <div>
-                    <a>Settings</a>
-                </div>
-            </nav>
-            <div className="content">
-                <div className = "imag">
-                <img src="https://avatanplus.com/files/resources/mid/5b4de991357f5164a8585f43.png" />
-                </div>
-                <div>
-                    ava + discription
-                </div>
-                <div>
-                    My posts
-                    <div>
-                        New post
-                    </div>
-                    <div>
-                        Post1
-                    </div>
-                    <div>
-                        Post2
-                    </div>
-                </div>
-            </div>
+            <Header />
+            <Nav />
+            <Container />
         </div>
-    );
-}
-
+    );}
 export default App;
-
