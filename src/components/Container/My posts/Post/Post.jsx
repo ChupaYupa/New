7c07a9f94a7352a60198e5fe0cd43c362aad5aa9
Toma <docsperj@gmail.com>
2019-11-08 17:36:53 +0300

    created props

--- src/components/Container/My posts/Post/Post.jsx
+++ src/components/Container/My posts/Post/Post.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import s from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
     return (
         <div className={s.item}>
             <img src="https://sovietgames.su/wp-content/uploads/2016/07/%D0%90%D0%B2%D0%B0%D1%82%D0%B0%D1%80%D0%BA%D0%B0.jpg" />
-            Post1
+            {props.message}
             <div>
-                <span>like</span>
+    <span>like:</span>{props.like}
             </div>
         </div>
     );
