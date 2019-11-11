bc5d7184ed023d4d4816de3712927e17503092c7
Toma <docsperj@gmail.com>
2019-11-11 07:28:33 +0300

    add route, browser-route

--- src/App.js
+++ src/App.js
@@ -3,17 +3,26 @@ import './App.css';
 import Header from './components/Header/Header';
 import Nav from './components/Nav/Nav';
 import Dialogs from './components/Dialogs/Dialogs';
-//import Container from './components/Container/Container';
+import Container from './components/Container/Container';
+import {BrowserRouter, Route} from 'react-router-dom';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
 
 const App = (props) => {
     return (
+        <BrowserRouter>
         <div className="app-wrapper">
             <Header />
             <Nav />
-            <div className="app-wrapper-content">
-            <Dialogs />
+        <div className="app-wrapper-content">
+            <Route path='/profile' component={Container} />
+            <Route path='/messages' component={Dialogs} />
+            <Route path='/news' component={News}/>
+            <Route path='/music' component={Music} />
+            <Route path='/music' component={Settings} />
             </div>
-           {/* <Container />*/}
         </div>
+        </BrowserRouter>
     );}
 export default App;
