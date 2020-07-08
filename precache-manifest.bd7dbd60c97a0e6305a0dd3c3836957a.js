395337696ef1ffd9ab345ad7c84ae4457b211c2c
Toma_Makarycheva <docsperj@gmail.com>
2020-07-08 20:01:29 +0300

    Updates

---
+++ precache-manifest.bd7dbd60c97a0e6305a0dd3c3836957a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceb64b85cfcc22dfee21abaaf9d037d3",
+    "url": "/New/index.html"
+  },
+  {
+    "revision": "0067cea637489c574d8f",
+    "url": "/New/static/css/3.5a322790.chunk.css"
+  },
+  {
+    "revision": "e5b2d60e45101946bee1",
+    "url": "/New/static/css/4.36ce94ef.chunk.css"
+  },
+  {
+    "revision": "97c50ac91f762c94aefb",
+    "url": "/New/static/css/5.834e176a.chunk.css"
+  },
+  {
+    "revision": "c789e56cead0996987ee",
+    "url": "/New/static/css/main.1f807ea8.chunk.css"
+  },
+  {
+    "revision": "83dfae54b1bd570f6801",
+    "url": "/New/static/js/2.1bc9bc51.chunk.js"
+  },
+  {
+    "revision": "0067cea637489c574d8f",
+    "url": "/New/static/js/3.27cbba0e.chunk.js"
+  },
+  {
+    "revision": "e5b2d60e45101946bee1",
+    "url": "/New/static/js/4.9499f4f3.chunk.js"
+  },
+  {
+    "revision": "97c50ac91f762c94aefb",
+    "url": "/New/static/js/5.0782e791.chunk.js"
+  },
+  {
+    "revision": "c789e56cead0996987ee",
+    "url": "/New/static/js/main.1599a7d0.chunk.js"
+  },
+  {
+    "revision": "3b31a2609874f63b7812",
+    "url": "/New/static/js/runtime~main.b9274640.js"
+  },
+  {
+    "revision": "455a9619d4068feb6334966d425655c6",
+    "url": "/New/static/media/805.455a9619.gif"
+  }
+]);
